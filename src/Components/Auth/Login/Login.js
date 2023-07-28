e31689b74917853310968d1f99a55f2f5fcbe638
roshinys <roshinchaya@gmail.com>
2023-07-28 10:37:31 +0530

    added login functionality

--- src/Components/Auth/Login/Login.js
+++ src/Components/Auth/Login/Login.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../store/auth-action";
+import AuthBox from "../Auth/AuthBox";
+import AuthWrapper from "../Auth/AuthWrapper";
+import AuthForm from "../Auth/AuthForm";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const loginHandler = (email, password) => {
+    const user = { email, password };
+    dispatch(loginUser(user, navigate));
+  };
+  return (
+    <AuthBox>
+      <AuthWrapper>
+        <h3>Login here</h3>
+        <AuthForm onSubmit={loginHandler} />
+      </AuthWrapper>
+    </AuthBox>
+  );
 }
 
 export default Login;
